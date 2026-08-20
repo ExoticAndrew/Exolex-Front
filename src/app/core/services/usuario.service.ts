c9379c505668dcebfcbc6346ac7b7188f0c26b14
ExoticAndrew <andrewantunes61@gmail.com>
2026-08-20 01:38:08 -0300

    feat: adiciona service e model de Usuario para dropdown de equipe

---
+++ src/app/core/services/usuario.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { UsuarioResponse } from '../models/usuario.model';
+
+@Injectable({ providedIn: 'root' })
+export class UsuarioService {
+  private readonly apiUrl = `${environment.apiUrl}/usuarios`;
+
+  constructor(private http: HttpClient) {}
+
+  listar(): Observable<UsuarioResponse[]> {
+    return this.http.get<UsuarioResponse[]>(this.apiUrl);
+  }
+}
